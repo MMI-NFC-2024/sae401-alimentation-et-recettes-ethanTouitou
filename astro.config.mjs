9caeb90763ba908a4da3ab2bba963143a55aaa89
ethanTouitou <150148202+ethanTouitou@users.noreply.github.com>
2026-03-23 10:42:07 +0100

    astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,19 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
+import node from '@astrojs/node';
 import tailwindcss from '@tailwindcss/vite';
 
-// https://astro.build/config
 export default defineConfig({
+  site: 'https://nutriguide.ethantouitou.fr',
+
+  adapter: node({
+    mode: 'standalone'
+  }),
+
+  security: {
+    checkOrigin: false
+  },
+
   vite: {
     plugins: [tailwindcss()]
   }
